967225b9b8a57c48593f57b0a855e2f006b5448a
ks-iitjmu <2023uma0221@iitjammu.ac.in>
2025-11-12 19:07:05 +0530

    model working

--- p_inference_classifier.py
+++ p_inference_classifier.py
@@ -43,19 +43,20 @@ while True:
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
+        # Use only the first hand to match training data
+        hand_landmarks = results.multi_hand_landmarks[0]
+        for i in range(len(hand_landmarks.landmark)):
+            x = hand_landmarks.landmark[i].x
+            y = hand_landmarks.landmark[i].y
 
-                x_.append(x)
-                y_.append(y)
+            x_.append(x)
+            y_.append(y)
 
-            for i in range(len(hand_landmarks.landmark)):
-                x = hand_landmarks.landmark[i].x
-                y = hand_landmarks.landmark[i].y
-                data_aux.append(x - min(x_))
-                data_aux.append(y - min(y_))
+        for i in range(len(hand_landmarks.landmark)):
+            x = hand_landmarks.landmark[i].x
+            y = hand_landmarks.landmark[i].y
+            data_aux.append(x - min(x_))
+            data_aux.append(y - min(y_))
 
         x1 = int(min(x_) * W) - 10
         y1 = int(min(y_) * H) - 10
